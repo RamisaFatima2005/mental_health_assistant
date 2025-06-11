18223dc87eff21301c82df365a6699492c3a5623
Ramisa <ramisafatima67@gmail.com>
2025-06-12 01:15:46 +0500

    Initial Commit

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ app = FastAPI()
 # Allow CORS from frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://mental-health-assistant-mu.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
